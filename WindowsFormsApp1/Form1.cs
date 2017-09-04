d588f25714763361d00b1f7a9ac505e5297544e7
Bram Stoof <bramstoof@live.com>
2017-09-04 12:35:41 +0200

    First build of the GUI

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,5 +16,70 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblWatt_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTime_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblEnergy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblResistence_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblDistence_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblSpeed_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblRoundMin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPulse_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
