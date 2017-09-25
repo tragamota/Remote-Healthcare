5107f61cb64dd5ee2d3c517741f4c542f0e8de18
Ian van de poll <ian.vd.poll@gmail.com>
2017-09-25 14:47:01 +0200

    Update Server

--- WindowsFormsApp1/Kettler.cs
+++ WindowsFormsApp1/Kettler.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using UserData;
 
 namespace Remote_Healtcare_Console {
     abstract class Kettler {
@@ -47,32 +48,3 @@ namespace Remote_Healtcare_Console {
             }
         }
     }
-
-    [Serializable]
-    public struct BikeData {
-        public int Pulse { get; set; }
-        public int Rpm { get; set; }
-        public int Distance { get; set; }
-        public int Resistance { get; set; }
-        public int Power { get; set; }
-        public int Energy { get; set; }
-        public TimeSpan Time { get; set; }
-        public double Speed { get; set; }
-
-        public BikeData(int pulse, int rpm, string speed, int distance, int resistance, int energy, string time, int power) {
-            Pulse = pulse;
-            Rpm = rpm;
-            Distance = distance;
-            Resistance = resistance;
-            Power = power;
-            Energy = energy;
-            string[] timeSplitted = time.Split(':');
-            Time = new TimeSpan(0, int.Parse(timeSplitted[0]), int.Parse(timeSplitted[1]));
-            Speed = (double.Parse(speed) / 10);
-        }
-
-        public override string ToString() {
-            return $"{Pulse}-{Rpm}-{Speed}-{Distance}-{Resistance}-{Energy}-{Time.Minutes + ":" + Time.Seconds}-{Power}";
-        }
-    }
-}
