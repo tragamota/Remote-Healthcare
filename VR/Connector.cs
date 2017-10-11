1a28106413323f6e4a2c7429a4f754a18a2f3e9c
Zwen1111 <zwen12@msn.com>
2017-10-11 11:56:50 +0200

    Little fix

--- VR/Connector.cs
+++ VR/Connector.cs
@@ -697,45 +697,7 @@ namespace VR {
             
         }
 
-        public void AddWater()
-        {
-            dynamic message = new
-            {
-                id = "tunnel/send",
-                data = new
-                {
-                    dest = tunnelID,
-                    data = new
-                    {
-                        id = "scene/node/add",
-                        data = new
-                        {
-                            name = "water",
-                            components = new
-                            {
-                                transform = new
-                                {
-                                    position = (new int[3] { 1, 5, 1 }),
-                                    scale = 1,
-                                    rotation = (new int[3] { 0, 0, 0 })
-                                },
-                                water = new
-                                {
-                                    size = (new int[2] {256,256}),
-                                    resolution =  0.9
-                                }
-                            }
-                        }
-                    }
-                }
-            };
-
-            SendMessage(message);
-            JObject jObject = ReadMessage();    
-            
-}
-
-        public void LoadRoutes()
+        public void LoadScene()
         {
             string path = "";
 
@@ -763,6 +725,44 @@ namespace VR {
             {
                 route.Reload();
             }
+        }
+
+        public void AddWater()
+        {
+            dynamic message = new
+            {
+                id = "tunnel/send",
+                data = new
+                {
+                    dest = tunnelID,
+                    data = new
+                    {
+                        id = "scene/node/add",
+                        data = new
+                        {
+                            name = "water",
+                            components = new
+                            {
+                                transform = new
+                                {
+                                    position = (new int[3] { 1, 5, 1 }),
+                                    scale = 1,
+                                    rotation = (new int[3] { 0, 0, 0 })
+                                },
+                                water = new
+                                {
+                                    size = (new int[2] { 256, 256 }),
+                                    resolution = 0.9
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            SendMessage(message);
+            JObject jObject = ReadMessage();
+
         }
     }
 }
